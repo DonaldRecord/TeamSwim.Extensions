393e2e33eaba67fa0e8a24c524f43fef95503650
Donald Record <donald.record@gmail.com>
2020-09-05 23:49:26 -0400

    Update YAML with csproj paths.

--- Source/TeamSwim.Extensions/System/Int32_Nullable/TryParse.cs
+++ Source/TeamSwim.Extensions/System/Int32_Nullable/TryParse.cs
@@ -8,9 +8,11 @@ namespace System
     public static partial class NullableInt32Ext
     {
         /// <summary>
-        ///     Converts the string representation of a number to its nullable 
-        ///     32-bit signed integer equivalent. 
+        ///     Converts the string representation of a number to its nullable 32-bit signed integer equivalent. 
         ///     A return value indicates whether the conversion succeeded.
+        /// <para>
+        ///     A <see langword="null"/> value will return <see langword="true" />.
+        /// </para>
         /// </summary>
         /// <param name="s">A string containing a number to convert.</param>
         /// <param name="result">When this method returns, contains the 32-bit signed integer value equivalent of the number contained in s, if the conversion succeeded, or null if the conversion failed. The conversion fails if the s parameter is null or System.String.Empty, is not of the correct format, or represents a number less than System.Int32.MinValue or greater than System.Int32.MaxValue. This parameter is passed uninitialized.</param>
@@ -23,20 +25,15 @@ namespace System
                 result = null;
                 return true;
             }
-            else
+
+            if (Int32.TryParse(s, out var tmp))
             {
-                int tmp;
-                if (Int32.TryParse(s, out tmp))
-                {
-                    result = tmp;
-                    return true;
-                }
-                else
-                {
-                    result = null;
-                    return false;
-                }
+                result = tmp;
+                return true;
             }
+            
+            result = null;
+            return false;
         }
     }
 }
