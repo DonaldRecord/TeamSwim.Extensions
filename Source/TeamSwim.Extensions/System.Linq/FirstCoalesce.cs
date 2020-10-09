d5b4909412f93146e3bb1125d891d6db22aa6032
Donald Record <donald.record@gmail.com>
2020-10-09 10:47:47 -0400

    Add System.Linq stuff

---
+++ Source/TeamSwim.Extensions/System.Linq/FirstCoalesce.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Linq
+{
+    partial class LinqExt
+    {
+        public static T FirstCoalesce<T>(this IEnumerable<T> source, params Func<T, bool>[] predicates)
+        {
+            var matches = new List<Tuple<int, T>>();
+
+            foreach (var elem in source)
+            {
+                var i = 0;
+                foreach (var predicate in predicates)
+                {
+                    var match = predicate.Invoke(elem);
+                    if (match)
+                    {
+                        if (i == 0)
+                            return elem;
+                        else
+                            matches.Add(new Tuple<int, T>(i, elem));
+                    }
+
+                    i++;
+                }
+            }
+
+            if (matches.Any())
+                return matches.OrderBy(m => m.Item1).First().Item2;
+            else 
+                return default;
+        }
+    }
+}
---
+++ Source/TeamSwim.Extensions/System.Linq/ThenPrioritize.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace System.Linq
+{
+    partial class LinqExt
+    {
+        /// <summary>
+        ///     Orders [already ordered] elements against a predicate, prioritizing elements that evaluate to <see langword="true"/> by putting them first.
+        ///     Error handling is not done for running the predicate.
+        /// </summary>
+        /// <typeparam name="TElement">Type of element.</typeparam>
+        /// <param name="source">Set of elements.</param>
+        /// <param name="predicate">Predicate to evaluate elements against.</param>
+        /// <returns></returns>
+        [Pure, NotNull, LinqTunnel]
+        [PublicAPI]
+        public static IOrderedEnumerable<TElement> ThenPrioritize<TElement>(
+            [NotNull] this IOrderedEnumerable<TElement> source,
+            [NotNull] Func<TElement, bool> predicate)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+            var result = source.ThenByDescending(predicate);
+            return result;
+        }
+    }
+}
