84df5b12e651d4943cbb7072c48c40b6b2219742
Donald Record <donald.record@gmail.com>
2020-10-15 16:02:09 -0400

    SubstringAfterFirstOccurrence

---
+++ Source/TeamSwim.Extensions.Tests/Methods/System/String/SubstringAfterFirstOccurrenceTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Methods.System.String
+{
+    [TestClass]
+    public class SubstringAfterFirstOccurrenceTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Throws_Expected_Null_Reference_Exception()
+        {
+            string str = null;
+            var actual = str.SubstringAfterFirstOccurrence("b");
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Throws_Expected_Argument_Null_Exception()
+        {
+            var actual = "abc".SubstringAfterFirstOccurrence(null);
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Returns_Expected_Value()
+        {
+            var actual = "abc".SubstringAfterFirstOccurrence("AB", StringComparison.OrdinalIgnoreCase);
+            Assert.AreEqual("c", actual);
+        }
+
+        [TestMethod]
+        public void Returns_Expected_Null_Value()
+        {
+            var actual = "abc".SubstringAfterFirstOccurrence("d", StringComparison.OrdinalIgnoreCase);
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void No_Errors_For_Zero_Width_String()
+        {
+            var actual = "".SubstringAfterFirstOccurrence("d", StringComparison.OrdinalIgnoreCase);
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void Zero_Width_Argument_Always_Returns_Null()
+        {
+            var actual = "".SubstringAfterFirstOccurrence("", StringComparison.OrdinalIgnoreCase);
+            Assert.IsNull(actual);
+        }
+    }
+}
---
+++ Source/TeamSwim.Extensions/Methods/System/String/SubstringAfterFirstOccurrence.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+
+namespace System
+{
+    partial class StringExt
+    {
+        /// <summary>
+        ///     (Opinionated)
+        ///     Returns a substring starting at the beginning of the original string until the end of the first specified occurrence.
+        ///     If the substring is not found in the source string, <see langword="null"/> is returned.
+        /// <para>
+        ///     Zero-length <paramref name="occurrence"/> argument will always return <see langword="null"/>.
+        /// </para>
+        /// </summary>
+        /// <param name="value">String to return substring from.</param>
+        /// <param name="occurrence">Where to end the substring.</param>
+        /// <param name="stringComparison">String Comparison to use when finding the occurrence.</param>
+        /// <returns></returns>
+        [PublicAPI]
+        [Pure, CanBeNull]
+        public static string SubstringAfterFirstOccurrence(
+            [NotNull] this string value,
+            [NotNull] string occurrence,
+            StringComparison stringComparison = StringComparison.CurrentCulture)
+        {
+            if (value == null) throw new NullReferenceException();
+            if (occurrence == null) throw new ArgumentNullException(nameof(occurrence));
+            if (occurrence.Length == 0)
+                return null;
+
+            var indexOf = value.IndexOf(occurrence, stringComparison);
+            if (indexOf == -1)
+                return null;
+
+            var start = indexOf + occurrence.Length;
+            var end = value.Length - start;
+            var result = value.Substring(start, end);
+            return result;
+        }
+    }
+}
