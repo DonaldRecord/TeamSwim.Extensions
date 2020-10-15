a0a5e6a36cc8f723e9c0266ec18df3d8f2a41351
Donald Record <donald.record@gmail.com>
2020-10-15 12:00:22 -0400

    Remove NCrunch cache files from index.

---
+++ Source/TeamSwim.Extensions/TeamSwimExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Text;
+
+namespace TeamSwim
+{
+    /// <summary>
+    ///     Provides metadata for the TeamSwim Extensions project.
+    /// </summary>
+    public static class TeamSwimExtensions
+    {
+        /// <summary>
+        ///     Tag for the TeamSwim Extensions Assembly
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        public static Assembly Assembly { get; } = typeof(TeamSwimExtensions).Assembly;
+    }
+}
