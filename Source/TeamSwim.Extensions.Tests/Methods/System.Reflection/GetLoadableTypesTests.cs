feac20a3cbecc08875579d749588cadd8e83ac85
Donald Record <donald.record@gmail.com>
2021-01-07 01:20:37 -0500

    ToCircularEnumerable and GetLoadableTypes added.

---
+++ Source/TeamSwim.Extensions.Tests/Methods/System.Reflection/GetLoadableTypesTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Methods.System.Reflection
+{
+    [TestClass]
+    public class GetLoadableTypesTests
+    {
+    }
+}
---
+++ Source/TeamSwim.Extensions/Methods/System.Reflection/Assembly/GetLoadableTypes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+using TeamSwim;
+
+namespace System.Reflection
+{
+    //https://stackoverflow.com/questions/7889228/how-to-prevent-reflectiontypeloadexception-when-calling-assembly-gettypes
+
+    partial class AssemblyExt
+    {
+        /// <summary>
+        ///     Get the loadable types in an assembly incase a <see cref="ReflectionTypeLoadException"/> occurs.
+        /// </summary>
+        /// <param name="assembly">Assembly to search for types.</param>
+        /// <returns>Array of loadable types.</returns>
+        [PublicAPI]
+        [Pure, NotNull, ItemNotNull]
+        public static Type[] GetLoadableTypes([NotNull] this Assembly assembly)
+        {
+            if (assembly == null) throw Exceptions.NullRef();
+            Type[] result;
+
+            try
+            {
+                result = assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                result = ex.Types.Where(t => t != null).ToArray();
+            }
+
+            return result;
+        }
+    }
+}
