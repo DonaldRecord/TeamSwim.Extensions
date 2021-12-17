2bf07ff3074c3425c5b4ca2c55675e0bb3136dca
Donald Record <donald.record@gmail.com>
2021-12-17 00:56:42 -0500

    Add CancellationToken.AsCompletedTask

---
+++ Source/TeamSwim.Extensions.Tests/Methods/System.Threading/CancellationToken/AsCompletedTaskTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Threading
+{
+    [TestClass]
+    public class AsCompletedTaskTests
+    {
+        [TestMethod]
+        public void Returns_Completed_Task()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var result = token.AsCompletedTask();
+
+            Assert.IsInstanceOfType(result, typeof(Task));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OperationCanceledException))]
+        public void Throws_Expected_Error()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            cts.Cancel();
+
+            var result = token.AsCompletedTask();
+
+            Assert.Fail();
+        }
+    }
+}
---
+++ Source/TeamSwim.Extensions/Methods/System.Threading/CancellationToken/_CancellationTokenExt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Threading
+{
+    /// <summary>
+    ///     Extension method class for <see cref="CancellationToken"/>.
+    /// </summary>
+    public static partial class CancellationTokenExt { }
+}
