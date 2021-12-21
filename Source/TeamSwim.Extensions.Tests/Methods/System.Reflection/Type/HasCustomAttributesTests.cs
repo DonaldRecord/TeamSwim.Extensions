8594e39af26e671e17de4d595ba1a9a4326f9523
Donald Record <donald.record@gmail.com>
2021-12-21 09:53:40 -0500

    Add HasCustomAttribute

---
+++ Source/TeamSwim.Extensions.Tests/Methods/System.Reflection/Type/HasCustomAttributesTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Reflection
+{
+    [TestClass]
+    public class HasCustomAttributesTests
+    {
+        [TestMethod]
+        public void Returns_Expected_Single_Results()
+        {
+            var actual = typeof(WithSingleAttribute).HasCustomAttributes<TestSingleAttribute>(out var attr);
+            Assert.IsTrue(actual);
+            Assert.AreEqual(1, attr.Count);
+            Assert.IsNotNull(attr);
+
+            actual = typeof(WithoutSingleAttribute).HasCustomAttributes(out attr);
+            Assert.IsFalse(actual);
+            Assert.AreEqual(0, attr.Count);
+            Assert.IsNotNull(attr);
+        }
+
+        [TestMethod]
+        public void Returns_Expected_Multiple_Results()
+        {
+            var actual = typeof(WithOneMultipleAttribute).HasCustomAttributes<TestMultipleAttribute>(out var attr);
+            Assert.AreEqual(1, attr.Count);
+            Assert.IsNotNull(attr);
+
+            actual = typeof(WithoutMultipleAttribute).HasCustomAttributes(out attr);
+            Assert.IsFalse(actual);
+            Assert.IsNotNull(attr);
+
+            actual = typeof(WithManyMultipleAttribute).HasCustomAttributes(out attr);
+            Assert.AreEqual(3, attr.Count);
+            Assert.IsNotNull(attr);
+        }
+
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+        class TestSingleAttribute : Attribute { }
+        [TestSingle] class WithSingleAttribute { }
+        class WithoutSingleAttribute { }
+
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+        class TestMultipleAttribute : Attribute { }
+        [TestMultiple] class WithOneMultipleAttribute { }
+        [TestMultiple, TestMultiple, TestMultiple] class WithManyMultipleAttribute { }
+        class WithoutMultipleAttribute { }
+    }
+}
---
+++ Source/TeamSwim.Extensions/Methods/System.Reflection/Type/HasCustomAttribute.cs
@@ -0,0 +1,36 @@
+﻿using JetBrains.Annotations;
+
+namespace System.Reflection
+{
+    partial class TypeExt
+    {
+        /// <summary>
+        ///     Determines if a custom attribute of a specified type is applied to a specified member.
+        /// </summary>
+        /// <typeparam name="TAttribute">Attribute type to seek on <paramref name="type"/>.</typeparam>
+        /// <param name="type">Type to be seeked for parameter.</param>
+        /// <param name="attribute">The custom attribute, if found.</param>
+        /// <returns><see langword="true"/> if the attribute exists on the type. Otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="AmbiguousMatchException">Throws when multiple attributes of same type are found.</exception>
+        [PublicAPI]
+        public static bool HasCustomAttribute<TAttribute>(
+            [NotNull] this Type type, 
+            [CanBeNull] out TAttribute attribute) where TAttribute : Attribute
+        {
+            attribute = type.GetCustomAttribute<TAttribute>();
+            var result = attribute != null;
+            return result;
+        }
+
+        /// <summary>
+        ///     Determines if a custom attribute of a specified type is applied to a specified member.
+        /// </summary>
+        /// <typeparam name="TAttribute">Attribute type to seek on <paramref name="type"/>.</typeparam>
+        /// <param name="type">Type to be seeked for parameter.</param>
+        /// <returns><see langword="true"/> if the attribute exists on the type. Otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="AmbiguousMatchException">Throws when multiple attributes of same type are found.</exception>
+        [PublicAPI]
+        public static bool HasCustomAttribute<TAttribute>([NotNull] this Type type) where TAttribute : Attribute
+            => type.HasCustomAttribute<TAttribute>(out _);
+    }
+}
