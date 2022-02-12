03ba7b637afd32141015ea121dc6e44b474d9cf3
Donald Record <donald.record@gmail.com>
2022-02-12 16:05:48 -0500

    Add initial TypeNameFormatter

---
+++ Source/TeamSwim.Extensions.Tests/Classes/TypeNameFormatterTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Classes
+{
+    [TestClass]
+    public class TypeNameFormatterTests
+    {
+        [TestMethod]
+        public void Expected_Default_Values()
+        {
+            AssertValue(typeof(string), "string", TypeNameFormattingOptions.Default);
+            AssertValue(typeof(List<string>), "List<string>", TypeNameFormattingOptions.Default);
+            AssertValue(typeof(Dictionary<string, object>), "Dictionary<string, object>", TypeNameFormattingOptions.Default);
+            AssertValue(typeof(Dictionary<,>), "Dictionary<TKey, TValue>", TypeNameFormattingOptions.Default);
+            AssertValue(typeof(List<>), "List<T>", TypeNameFormattingOptions.Default);
+            AssertValue(typeof(IDynamicMetaObjectProvider), "dynamic", TypeNameFormattingOptions.Default);
+            AssertValue(typeof(StringComparison), "StringComparison", TypeNameFormattingOptions.Default);
+            AssertValue(typeof(int[]), "int[]", TypeNameFormattingOptions.Default);
+            AssertValue(typeof(DynamicObject), "dynamic", TypeNameFormattingOptions.Default);
+        }
+
+        [TestMethod]
+        public void Expected_Enum_Values()
+        {
+            AssertValue(typeof(StringComparison), "Enum", TypeNameFormattingOptions.UseEnumKeyword);
+            AssertValue(typeof(StringComparison?), "Nullable<Enum>", TypeNameFormattingOptions.UseEnumKeyword);
+            AssertValue(typeof(StringComparison?), "Enum?", TypeNameFormattingOptions.UseEnumKeyword | TypeNameFormattingOptions.UseQuestionMarkForNullable);
+        }
+
+        [TestMethod]
+        public void Expected_Fully_Qualified_Values()
+        {
+            AssertValue(typeof(string), typeof(string).FullName, TypeNameFormattingOptions.FullyQualified);
+            AssertValue(typeof(List<string>), "System.Collections.Generic.List<System.String>", TypeNameFormattingOptions.FullyQualified);
+            AssertValue(typeof(Dictionary<string, object>), "System.Collections.Generic.Dictionary<System.String, System.Object>", TypeNameFormattingOptions.FullyQualified);
+            AssertValue(typeof(Dictionary<,>), "System.Collections.Generic.Dictionary<,>", TypeNameFormattingOptions.FullyQualified);
+            AssertValue(typeof(List<>), "System.Collections.Generic.List<>", TypeNameFormattingOptions.FullyQualified);
+            AssertValue(typeof(IDynamicMetaObjectProvider), "System.Dynamic.IDynamicMetaObjectProvider", TypeNameFormattingOptions.FullyQualified);
+            AssertValue(typeof(StringComparison), "System.StringComparison", TypeNameFormattingOptions.FullyQualified);
+            AssertValue(typeof(int[]), "System.Int32[]", TypeNameFormattingOptions.FullyQualified);
+            AssertValue(typeof(DynamicObject), "System.Dynamic.DynamicObject", TypeNameFormattingOptions.FullyQualified);
+        }
+
+
+        private void AssertValue(Type type, string expected, TypeNameFormattingOptions options)
+        {
+            var actual = TypeNameFormatter.GetName(type, options);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Source/TeamSwim.Extensions/Classes/TypeNameFormattingOptions.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.Dynamic;
+using System.Reflection;
+
+namespace System
+{
+    /// <summary>
+    ///     Options available for special type name formatting options [used in <see cref="TypeNameFormatter"/>].
+    /// </summary>
+    [Flags]
+    public enum TypeNameFormattingOptions
+    {
+        /// <summary>
+        ///     Returns the result of the type's <see cref="MemberInfo.Name"/>.
+        /// </summary>
+        [Description("Use name only")]
+        NameOnly = 0,
+
+        /// <summary>
+        ///     Replaces "Nullable&lt;&gt;" with "?". E.G. returns "Int32?" instead of "Nullable&lt;Int32&gt;".
+        /// </summary>
+        [Description("Use question mark for nullable types")]
+        UseQuestionMarkForNullable = 1,
+
+        /// <summary>
+        ///     Returns .NET keyword aliases instead of type names. E.G. "string" instead of "String".
+        /// <para>
+        ///     Using this option overrides the <see cref="FullyQualified"/> option
+        ///     for types that have a keyword associated.
+        /// </para>
+        ///     For enumeration types, the <see cref="UseEnumKeyword"/> must be used to have "enum" returned.
+        ///     Otherwise, this flag will only return "enum" when the type being passed in is strictly <see cref="Enum"/>.
+        /// </summary>
+        [Description("Use keywords")]
+        UseKeywords = 2,
+
+        /// <summary>
+        ///     Returns the fully qualified type name for the main type and all of the nested types.
+        ///     <code>System.Collections.Generic.List&lt;System.String&gt;</code>
+        /// <para>
+        ///     If the type has a keyword representing it, this option is overridden by <see cref="UseKeywords"/>.
+        /// </para>
+        /// </summary>
+        [Description("Use fully qualified type name")]
+        FullyQualified = 4,
+
+        /// <summary>
+        ///     Returns "enum" for any Enumeration Type. 
+        ///     Returns "System.Enum" when the <see cref="FullyQualified"/> option is also present.
+        /// </summary>
+        [Description(@"Use ""enum"" keyword")]
+        UseEnumKeyword = 8,
+
+        /// <summary>
+        ///     Returns the name of <see cref="DynamicObject"/> for any object implementing <see cref="IDynamicMetaObjectProvider"/>.
+        /// <para>
+        ///     This options respects the <see cref="FullyQualified"/> and <see cref="UseKeywords"/> options.
+        /// </para>
+        /// </summary>
+        [Description("Use dynamic keyword")]
+        UseDynamicKeyword = 16,
+
+        /// <summary>
+        ///     Default formatting option. Combination of 
+        ///     <see cref="UseQuestionMarkForNullable"/> | 
+        ///     <see cref="UseKeywords"/> |
+        ///     <see cref="UseDynamicKeyword"/>.
+        /// </summary>
+        [Description("Use default type name formatting options")]
+        Default = 19,
+    }
+}
