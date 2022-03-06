f0dc3d60a6021f7d0a0d5762749bfad194d67193
Donald Record <donald.record@gmail.com>
2022-03-06 15:09:31 -0500

    Enumerable Bool methods

---
+++ Source/TeamSwim.Extensions/Methods/System.Linq/Bool/All.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+    partial class LinqExt
+    {
+        /// <summary>
+        ///     Determines if all elements are <see langword="true"/>.
+        /// </summary>
+        /// <param name="source">Sequence of <see cref="bool"/>.</param>
+        /// <returns><see langword="true"/> if all elements are <see langword="null"/>. Otherwise, <see langword="false"/>.</returns>
+        public static bool All(this IEnumerable<bool> source) => source.All(_ => _);
+    }
+}
---
+++ Source/TeamSwim.Extensions/Methods/System.Linq/Bool/Any.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+    partial class LinqExt
+    {
+        /// <summary>
+        ///     Determines whether any element is <see langword="true"/>.
+        /// </summary>
+        /// <param name="source">Sequence of <see cref="bool"/>.</param>
+        /// <returns><see langword="true"/> if any element is <see langword="null"/>. Otherwise, <see langword="false"/>.</returns>
+        public static bool Any(this IEnumerable<bool> source) => source.Any(_ => _);
+    }
+}
