3807467ffba9912d684c227c9956b2ab59a334a6
Donald Record <donald.record@gmail.com>
2022-05-29 13:22:29 -0400

    None

---
+++ Source/TeamSwim.Extensions.Tests/Methods/System.Linq/NoneTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Linq
+{
+    [TestClass]
+    public class NoneTests
+    {
+        [TestMethod]
+        public void Expected_True()
+        {
+            Assert.IsTrue(new List<int> { 1, 2, 3, 4, 5 }.None(i => i == 6));
+        }
+
+        [TestMethod]
+        public void Expected_False()
+        {
+            Assert.IsTrue(new List<int> { 1, 2, 3, 4, 5 }.None(i => i == 6));
+        }
+
+        [TestMethod]
+        public void Expected_Error()
+        {
+            List<int> list = null;
+            Func<int, bool> predicate = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => list.None(i => i == 1));
+            Assert.ThrowsException<ArgumentNullException>(() => new List<int>().None(predicate));
+        }
+    }
+}
---
+++ Source/TeamSwim.Extensions/Methods/System.Linq/None.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Linq
+{
+    partial class LinqExt
+    {
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        [PublicAPI]
+        [Pure]
+        public static bool None<T>(
+            [NotNull] this IEnumerable<T> source, 
+            [NotNull] Func<T, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            var result = source.All(t => !predicate.Invoke(t));
+            return result;
+        }
+    }
+}
