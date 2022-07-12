63c7918f6ff98b0cb6228d44358886d4da74eeac
Donald Record <donald.record@gmail.com>
2022-07-12 15:42:49 -0400

    Add RemoveFromEnding

---
+++ Source/TeamSwim.Extensions.Tests/Methods/System/String/RemoveFromEndingTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class RemoveFromBeginningTests
+{
+    [TestMethod]
+    public void Throws_Expected_Exceptions()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => ((string)null).RemoveFromEnding(""));
+        Assert.ThrowsException<ArgumentNullException>(() => "".RemoveFromEnding(null));
+    }
+
+    [TestMethod]
+    public void Assert_String_Empty_Value_ReferenceEquals()
+    {
+        var value = "";
+        var result = value.RemoveFromEnding("asdf");
+        Assert.IsTrue(ReferenceEquals(value, result));
+    }
+
+    [TestMethod]
+    public void Assert_String_Empty_Substring_ReferenceEquals()
+    {
+        var value = "asdf";
+        var result = value.RemoveFromEnding("");
+        Assert.IsTrue(ReferenceEquals(value, result));
+    }
+
+    [TestMethod]
+    public void Assert_Expected_Results()
+    {
+        void AssertValue(string value, string substring, StringComparison stringComparison, string expected)
+        {
+            var actual = value.RemoveFromEnding(substring, stringComparison);
+            Assert.AreEqual(expected, actual);
+        }
+
+        AssertValue("AAABBB", "BBB", StringComparison.Ordinal, "AAA");
+        AssertValue("AAABBB", "bbb", StringComparison.OrdinalIgnoreCase, "AAA");
+        AssertValue("ccc", "aaa", StringComparison.Ordinal, "ccc");
+        AssertValue("a", "a", StringComparison.Ordinal, "");
+        AssertValue("ba", "a", StringComparison.Ordinal, "b");
+    }
+}
