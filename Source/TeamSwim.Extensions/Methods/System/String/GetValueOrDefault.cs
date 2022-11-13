b549f81b81bf1a2c0d0311bceaca26e3a8977e44
Donald Record <donald.record@gmail.com>
2022-11-13 17:36:32 -0500

    Revert namespace change

--- Source/TeamSwim.Extensions/Methods/System/String/GetValueOrDefault.cs
+++ Source/TeamSwim.Extensions/Methods/System/String/GetValueOrDefault.cs
@@ -1,32 +1,33 @@
 ﻿using JetBrains.Annotations;
 
-namespace System;
-
-partial class StringExt
+namespace System
 {
-    /// <summary>
-    ///     (Opinionated)
-    ///     Return a string's value if it is not <see langword="null"/> or white space.
-    ///     Otherwise, return a <paramref name="default"/>.
-    ///     If you want whitespace only values not to coalesce to default,
-    ///     use <paramref name="replaceOnlyWhiteSpaceString"/> parameter.
-    /// </summary>
-    /// <param name="value">String to check/return value for.</param>
-    /// <param name="default">Default value to coalesce to when string is null/empty/whitespace.</param>
-    /// <param name="replaceOnlyWhiteSpaceString">When <see langword="true"/>, </param>
-    /// <returns></returns>
-    [PublicAPI]
-    [Pure, ContractAnnotation("default:null=>null;default:notnull=>notnull")]
-    public static string GetValueOrDefault(
-        [CanBeNull] this string value, 
-        string @default, 
-        bool replaceOnlyWhiteSpaceString = true)
+    partial class StringExt
     {
-        var replace = replaceOnlyWhiteSpaceString
-            ? String.IsNullOrWhiteSpace(value)
-            : String.IsNullOrEmpty(value);
+        /// <summary>
+        ///     (Opinionated)
+        ///     Return a string's value if it is not <see langword="null"/> or white space.
+        ///     Otherwise, return a <paramref name="default"/>.
+        ///     If you want whitespace only values not to coalesce to default,
+        ///     use <paramref name="replaceOnlyWhiteSpaceString"/> parameter.
+        /// </summary>
+        /// <param name="value">String to check/return value for.</param>
+        /// <param name="default">Default value to coalesce to when string is null/empty/whitespace.</param>
+        /// <param name="replaceOnlyWhiteSpaceString">When <see langword="true"/>, </param>
+        /// <returns></returns>
+        [PublicAPI]
+        [Pure, ContractAnnotation("default:null=>null;default:notnull=>notnull")]
+        public static string GetValueOrDefault(
+            [CanBeNull] this string value, 
+            string @default, 
+            bool replaceOnlyWhiteSpaceString = true)
+        {
+            var replace = replaceOnlyWhiteSpaceString
+                ? String.IsNullOrWhiteSpace(value)
+                : String.IsNullOrEmpty(value);
 
-        var result = replace ? @default : value;
-        return result;
+            var result = replace ? @default : value;
+            return result;
+        }
     }
 }
