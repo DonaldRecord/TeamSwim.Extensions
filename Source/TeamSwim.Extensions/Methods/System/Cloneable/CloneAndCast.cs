7df865569c772122d4d5f886d651a543b3eb479a
Donald Record <donald.record@gmail.com>
2023-01-18 10:53:54 -0500

    Interim changes

---
+++ Source/TeamSwim.Extensions/Methods/System/Cloneable/CloneAndCast.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+namespace System
+{
+    partial class CloneableExt
+    {
+        /// <summary>
+        ///     (Opinionated)
+        ///     Clone an object and return the cloned value after casting it to the
+        ///     origin type of the original <paramref name="value"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [PublicAPI]
+        [Pure, NotNull]
+        public static T CloneAndCast<T>([NotNull] this T value) where T : ICloneable
+            => (T)value.Clone();
+    }
+}
---
+++ Source/TeamSwim.Extensions/Methods/System/Cloneable/_CloneableExt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    /// <summary>
+    ///     Extension method class for <see cref="ICloneable"/>
+    /// </summary>
+    public static partial class CloneableExt { }
+}
